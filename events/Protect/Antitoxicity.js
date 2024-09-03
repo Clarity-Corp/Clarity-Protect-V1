8af33a9d59b4bf1b8e4007f209283e7e29a57afa
Tsu <126349790+1Tsubasa@users.noreply.github.com>
2024-09-03 22:47:57 +0200

    Add files via upload

--- events/Protect/Antitoxicity.js
+++ events/Protect/Antitoxicity.js
@@ -1,63 +1,163 @@
 const { google } = require('googleapis');
-const API_KEY = "AIzaSyCrqBjlfdgKWeb5GGb2qbKisdhBLVb7wLc";
 const handleViolation = require("../../Structure/Util/handleViolation");
 const Logs = require("../../Structure/Db/Models/logs/Logs");
-const Owner = require("../../Structure/Db/Models/Buyer/Owner")
-const WhiteList = require('../../Structure/Db/Models/Whitelist/index')
+const WhiteList = require('../../Structure/Db/Models/Whitelist/index');
 const Antitoxicity = require('../../Structure/Db/Models/Protect/antitoxicity');
-const {PermissionsBitField} = require("discord.js");
+const { PermissionsBitField } = require("discord.js");
 const userWarnings = new Map();
 const usersWithToxicity = new Set();
+
+// Table de conversion leetspeak basée sur l'image fournie
+const leetMap = {
+    '4': 'A', '@': 'A', '/-\\': 'A',
+    '8': 'B',
+    '(': 'C', '<': 'C', '[': 'C', '{': 'C',
+    '|)': 'D', 'cl': 'D',
+    '3': 'E',
+    '|=': 'F', 'ph': 'F',
+    '6': 'G', '&': 'G', '9': 'G',
+    '#': 'H', '|-|': 'H', '[-]': 'H',
+    '1': 'I', '!': 'I', '|': 'I', 'eye': 'I',
+    '_|': 'J',
+    '|<': 'K', '1<': 'K',
+    '|_': 'L',
+    '|\\/|': 'M', '|V|': 'M', '/\\/\\': 'M', '^^': 'M', 'em': 'M',
+    '|\\|': 'N', '/\\/': 'N',
+    '0': 'O', '()': 'O', '[]': 'O',
+    '|>': 'P', '|D': 'P',
+    '0_': 'Q',
+    '|2': 'R',
+    '5': 'S', '$': 'S',
+    '7': 'T', '+': 'T', '|-': 'T',
+    '|_|': 'U', '\\/\\': 'U',
+    '\\/': 'V',
+    '\\/\\/': 'W', 'VV': 'W', '\\X/': 'W',
+    '><': 'X', '}{': 'X',
+    '¥': 'Y', '`/': 'Y',
+    '2': 'Z'
+};
+
+// Fonction pour convertir le leetspeak en texte normal
+function convertLeetSpeak(text) {
+    const regex = new RegExp(Object.keys(leetMap).map(leet => leet.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|'), 'g');
+    return text.replace(regex, match => leetMap[match] || match);
+}
+
 module.exports = {
     name: "messageCreate",
     once: false,
-    run: async(client, message) => {
+    run: async (client, message) => {
         if (!message.guild || message.author.id === client.user.id) return;
-        let data = await Antitoxicity.findOne({
-            where: { guildId: message.guild.id }
-        });
+
+        let data = await Antitoxicity.findOne({ where: { guildId: message.guild.id } });
         if (!data || !data.status) return;
+
         let member = message.guild.members.cache.get(message.author.id) || await message.guild.members.fetch(message.author.id).catch(() => null);
         if (!member) return;
+
         if (data.bypass_status) {
             if (data.wl_link) {
-                let wl_links = data.wl_link ? data.wl_link.split(",") : [];
+                let wl_links = data.wl_link.split(",");
                 for (const wlLink of wl_links) {
-                    if(message.content.includes(wlLink)) return;
+                    if (message.content.includes(wlLink)) return;
                 }
             }
-            if (data.wl_users) {
-                let wl_users = data.wl_users ? data.wl_users.split(",") : [];
-                if (wl_users.includes(message.author.id)) return;
-            }
-            if (data.wl_role) {
-                let wl_roles = data.wl_role ? data.wl_role.split(",") : [];
-                let hasRoleInWhitelist = wl_roles.length > 0 && message.member.roles.cache.some(role => wl_roles.includes(role.id));
-                if (hasRoleInWhitelist) return;
-            }
+            if (data.wl_users && data.wl_users.split(",").includes(message.author.id)) return;
+            if (data.wl_role && message.member.roles.cache.some(role => data.wl_role.split(",").includes(role.id))) return;
             if (data.use_botWl) {
                 let isWl = await WhiteList.findOne({
                     where: { guildId: message.guild.id, userId: message.author.id }
                 });
                 if (isWl) return;
             }
-            if (data.use_botOwner) {
-                let isOwn = client.functions.isOwn(client, message.author.id)
-                if (isOwn) return;
-            }
+            if (data.use_botOwner && client.functions.isOwn(client, message.author.id)) return;
         }
+
         if (data.permission_allowed) {
             let hasPerm = message.member.permissions.has(PermissionsBitField.Flags.Administrator) ||
                 message.member.permissions.has(PermissionsBitField.Flags.ManageMessages);
             if (hasPerm) return;
         }
 
-    }
-}
+        // Conversion du leetspeak en texte normal
+        const normalizedContent = convertLeetSpeak(message.content);
 
+        const perspectiveClient = await google.discoverAPI(client.config.protect.toxicity_discovery).catch(err => {
+            console.error('Erreur lors de la découverte de l\'API :', err);
+            throw err;
+        });
+
+        const analyzeRequest = {
+            comment: { text: normalizedContent }, // Analyse du texte normalisé
+            requestedAttributes: { TOXICITY: {} },
+            languages: ["en", "es", "fr", "de", "it", "ru", "pt"]
+        };
+
+        perspectiveClient.comments.analyze({
+            key: client.config.api_key.google_api,
+            resource: analyzeRequest
+        }).then(
+            async response => {
+                const toxicityScore = response.data.attributeScores.TOXICITY.summaryScore.value;
+                const toxicityThreshold = data.sensibility || 0.1;
+
+                if (toxicityScore >= toxicityThreshold) {
+                    await handleViolationAndWarnings(client, message, member, data, userWarnings, usersWithToxicity);
+                    if (data.logs_status) {
+                        if (data.logs) {
+                            let logsc = await client.channels.cache.get(data.logs);
+                            if (logsc) {
+                                logsc.send({
+                                    embeds: [{
+                                        color: client.color,
+                                        description: `**${message.author}** a envoyé un message non approprié.\n**Toxicité du message** : ${(toxicityScore * 100).toFixed(2)}%`,
+                                        timestamp: new Date(),
+                                        footer: client.footer,
+                                        fields: [{
+                                            name: 'Message',
+                                            value: normalizedContent
+                                        }],
+                                        author: {
+                                            name: message.author.tag,
+                                            icon_url: message.author.displayAvatarURL({ dynamic: true })
+                                        }
+                                    }]
+                                });
+                            }
+                        } else {
+                            let logs = await Logs.findOne({ where: { guildId: message.guild.id } });
+                            if (logs.logs_status) {
+                                let logsc = client.channels.cache.get(logs.logs_channel);
+                                if (logsc) {
+                                    logsc.send({
+                                        embeds: [{
+                                            color: client.color,
+                                            description: `**${message.author}** a envoyé un message non approprié.\n**Toxicité du message** : ${(toxicityScore * 100).toFixed(2)}%`,
+                                            timestamp: new Date(),
+                                            footer: client.footer,
+                                            fields: [{
+                                                name: 'Message',
+                                                value: normalizedContent
+                                            }],
+                                            author: {
+                                                name: message.author.tag,
+                                                icon_url: message.author.displayAvatarURL({ dynamic: true })
+                                            }
+                                        }]
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        ).catch(err => {
+            console.error('Erreur lors de l\'analyse de toxicité :', err);
+        });
+    }
+};
 
 async function handleViolationAndWarnings(client, message, member, data, userWarnings, usersWithToxicity) {
-    message.delete().catch(() => false);
     usersWithToxicity.add(message.author.id);
 
     let warnings = userWarnings.get(message.author.id) || 0;
@@ -66,7 +166,7 @@ async function handleViolationAndWarnings(client, message, member, data, userWar
 
     if (data.rep && warnings >= data.rep_limit) {
         userWarnings.delete(message.author.id);
-        await handleViolation(member, data.sanction, data.sanction_admin, "Clarity Anti-link system");
+        await handleViolation(member, data.sanction, data.sanction_admin, "Clarity Anti-toxicity system");
         usersWithToxicity.delete(message.author.id);
         await client.functions.tempMessage(message, `${message.author}, vous avez été sanctionné pour avoir envoyé un message contenant un taux de toxicité élevé. Avertissements : ${warnings}/${data.rep_limit}`, client);
     } else if (data.rep) {
@@ -74,4 +174,6 @@ async function handleViolationAndWarnings(client, message, member, data, userWar
     } else {
         await handleViolation(member, data.sanction, data.sanction_admin, "Clarity Anti-toxicity system");
     }
+
+    message.delete().catch(() => false);
 }
